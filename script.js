fc27805e49f4051dca86a6fbec679ff2b2ed8696
Iz-Dine Amadou <2088346@talnet.nl>
2020-03-24 15:45:12 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var h1 = document.querySelector('h1');
+var var h1 = document.querySelector('h1');
 h1.innerText = document.title;
 document.querySelector('h1')
 
-const h1 = document.createElement('h1');
-div.className = 'h1';
+
+h1.classList.add("classy");
